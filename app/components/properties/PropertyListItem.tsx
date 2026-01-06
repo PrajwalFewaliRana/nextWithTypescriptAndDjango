ee35e8e4170786b9b3af425bd133149991b9cee6
PrajwalFewaliRana <prajwalmagar68@gmail.com>
2026-01-06 23:26:44 +0545

    property detai page

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const PropertyListItem = () => {
   return (
-    <div>PropertyListItem</div>
-  )
-}
+    <div className="cursor-pointer">
+      <div className="relative overflow-hidden aspect-square rounded-xl">
+        <Image
+          fill
+          src="/beach_1.jpg"
+          sizes="(max-width:768px) 768px,(max-width:1200px):768px,768px"
+          className="hover:scale-110 object-cover transition h-full w-full"
+          alt="Beach House"
+        />
+      </div>
+      <div className="mt-2">
+        <p className="text-lg font-bold ">Property name</p>
+      </div>
+      <div className="mt-2">
+        <p className="text-sm text-gray-500"><strong>$200</strong> per night</p>
+      </div>
+    </div>
+  );
+};
 
-export default PropertyListItem
+export default PropertyListItem;
